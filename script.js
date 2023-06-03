4c995e3873cac918769371f1fc4f1fec6e43a842
aj-abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2023-06-03 21:54:02 +0400

    Make button clear and make new grid

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 const grid = document.querySelector(".grid");
+const button = document.querySelector("button.new");
+button.addEventListener("click", promptNewGrid);
 
-color = function (e) {
-    console.log(this);
+makeGrid(16);
+
+function promptNewGrid(e, msg = "New grid size: ") {
+    const gridSize = +prompt(msg);
+    if (isNaN(gridSize)) {
+        promptNewGrid(e, "Size must be a number. ");
+    }
+    else if (gridSize < 1 || gridSize > 100) {
+        promptNewGrid(e, "Maximum size is 100. ");
+    }
+    else {
+        clearGrid();
+        makeGrid(gridSize);
+    }
+}
+
+function color(e) {
     this.classList.add("colored");
 }
 
-gridSize = 16;
-for (let i = 0; i < gridSize**2; i++) {
-    const cell = document.createElement("div");
+function makeGrid(gridSize) {
+    for (let i = 0; i < gridSize**2; i++) {
+        const cell = document.createElement("div");
+        
+        cell.classList.add("cell");
+        cell.style.width = `${100/gridSize}%`;
+        cell.addEventListener("mouseover", color);
     
-    cell.classList.add("cell");
-    cell.style.width = `${100/gridSize}%`;
-    cell.addEventListener("mouseover", color);
-
-    grid.appendChild(cell);
+        grid.appendChild(cell);
+    }
 }
 
+function clearGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+}
