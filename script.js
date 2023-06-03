143c1d639be31ba07b1b14d9b2487098e03d8488
aj-abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2023-06-03 21:24:04 +0400

    Color on hover

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const grid = document.querySelector(".grid");
 
+color = function (e) {
+    console.log(this);
+    this.classList.add("colored");
+}
+
 gridSize = 16;
 for (let i = 0; i < gridSize**2; i++) {
     const cell = document.createElement("div");
+    
     cell.classList.add("cell");
     cell.style.width = `${100/gridSize}%`;
+    cell.addEventListener("mouseover", color);
+
     grid.appendChild(cell);
 }
+
